213aeb7d0d6576dcfcdd0a1dedce670e26b34c93
macen <ausmosismason@gmail.com>
2022-04-10 02:16:40 -0700

    Commands sub folder loading 👌

--- src/loader.js
+++ src/loader.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const path = require("path");
 const fs = require("fs");
+const { Console } = require('console');
 
 
 /**
@@ -38,39 +39,74 @@ class CommandLoader{
 
 
     loadCommands(){
-        //Todo add sub folders
 
         this.client.commands = new Discord.Collection()
 
-        if(this.options.showLogs) console.log(`Loading commands...`)
-        const commands = fs.readdirSync(`${this.commandPath}`).filter(file => file.endsWith(".js"))
+        if (this.options.showLogs) console.log(`Loading commands...`)
 
-        if(commands.length === 0 && this.options.showLogs) console.log(`[WARN] Command folder empty.`)
+        const localDir = fs.readdirSync(`${this.commandPath}`).filter(file => file.endsWith(".js"))
+        const empty = fs.readdirSync(`${this.commandPath}`)
+      
+        if (empty.length === 0 && this.options.showLogs) console.log(`[WARN] Command folder empty.`)
 
-        for(const file of commands) {
+        var i = 0
+        fs.readdirSync(`${this.commandPath}`).forEach(dirs => {
 
-            const command = require(path.join(require.main.path, this.commandPath, file))
-
-            try {
-
-                if(!command.name) Object.assign(command, { name: file.split('.')[0] })
-
-                this.client.commands.set(command.name.toLowerCase(), command)
-
-                if (this.options.showLogs) console.log(`-> Loaded command ${command.name.toLowerCase()}`)
+            if(dirs == localDir[i]){
                 
-                 
-            } catch (error) {
-                console.log(`Command file '${file.split('.')[0]}' Had a error loading. File Likely empty or is not a module.`)
-                console.log(error)
-            }
-    
-            delete require.cache[require.resolve(path.join(require.main.path, this.commandPath, file))]
-        }
-        if(this.options.showLogs) console.log(`All commands loaded!`)
+                const command = require(path.join(require.main.path, `${this.commandPath}`, localDir[i]))
 
+                try {
+
+                    if(!command.name) Object.assign(command, { name: localDir[i].split('.')[0] })
+
+                    this.client.commands.set(command.name.toLowerCase(), command)
+
+                    if(this.options.showLogs) console.log(`-> Loaded command ${command.name.toLowerCase()}`)
+
+
+                } catch (error) {
+                    console.log(`Command file '${localDir[i].split('.')[0]}' Had a error loading. File Likely empty or is not a module.`)
+                    console.log(error)
+                }
+
+                delete require.cache[require.resolve(path.join(require.main.path, `${this.commandPath}`, localDir[i]))]
+            
+                i++
+                return
+            }
+        
+            
+            const commands = fs.readdirSync(`${this.commandPath}/${dirs}`).filter(file => file.endsWith(".js"))
+
+            if(commands.length === 0 && this.options.showLogs) return console.log(`[WARN] Folder ${dirs} is empty.`)
+
+            for (const file of commands) {
+                const command = require(path.join(require.main.path, `${this.commandPath}/${dirs}`, file))
+
+                try {
+
+                    if(!command.name) Object.assign(command, { name: file.split('.')[0] })
+
+                    this.client.commands.set(command.name.toLowerCase(), command)
+
+                    if(this.options.showLogs) console.log(`-> Loaded command ${command.name.toLowerCase()}`)
+
+
+                } catch (error) {
+                    console.log(`Command file '${file.split('.')[0]}' Had a error loading. File Likely empty or is not a module.`)
+                    console.log(error)
+                }
+
+                delete require.cache[require.resolve(path.join(require.main.path, `${this.commandPath}/${dirs}`, file))]
+            }
+        })
+
+        if (this.options.showLogs) console.log(`All commands loaded!`)
     }
 
+
+    // Todo port commands code to events
     loadEvents(){
 
         if(this.options.showLogs) console.log(`Loading events...`)
